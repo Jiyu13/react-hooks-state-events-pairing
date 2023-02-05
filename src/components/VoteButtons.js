10220da1d4063e1d736b78e4d3b9db28faa678a3
Ziru Zou <jiyucoding@yahoo.com>
2023-02-05 10:16:04 -0500

    separate onClick event for upvote and downvote buttons

--- src/components/VoteButtons.js
+++ src/components/VoteButtons.js
@@ -5,15 +5,19 @@ function VoteButtons( {upvotes, downvotes} ) {
     const [upvotesCount, setUpVotesCount] = useState(upvotes)
     const [downvotesCount, setDownVotesCount] = useState(downvotes)
 
-    function handldeVote() {
+    function handldeUpVote() {
         setUpVotesCount(currentVotes => currentVotes + 1)
     }
 
+    function handldeDownVote() {
+
+        setDownVotesCount(currentVotes => currentVotes + 1)
+    }
 
     return (
         <>
-            <button id="upvotes" onClick={handldeVote}>{upvotesCount} 👍</button>
-            <button id="downvotes" onClick={handldeVote}>{downvotesCount} 👎</button>
+            <button id="upvotes" onClick={handldeUpVote}>{upvotesCount} 👍</button>
+            <button id="downvotes" onClick={handldeDownVote}>{downvotesCount} 👎</button>
         </>
         
     )
