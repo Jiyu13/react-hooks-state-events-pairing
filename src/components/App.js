170f2f095f4b72202f33090ec3f07ff3deaacd02
Ziru Zou <jiyucoding@yahoo.com>
2023-02-05 09:19:20 -0500

    show video details in DOM

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,36 @@ import video from "../data/video.js";
 
 function App() {
   console.log("Here's your data:", video);
+  const { id, title, embedUrl, views, createdAt, upvotes, downvotes, comments} = video
 
   return (
     <div className="App">
       <iframe
         width="919"
         height="525"
-        src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+        src={embedUrl}
         frameBorder="0"
         allowFullScreen
         title="Thinking in React"
       />
+      <h1>{title}</h1>
+      <div>{views} views | {createdAt}</div>
+      <button id="upvotes">{upvotes} 👍</button>
+      <button id="downvotes">{downvotes} 👎</button>
+      
+      <hr />
+
+      <button id="comments">Hide Comments</button>
+      <h2>{comments.length} comments</h2>
+      <ul>
+        {comments.map(comment => {
+          return <li key={comment.id} id={comment.id}>
+            <h3>{comment.user}</h3>
+            <p>{comment.comment}</p>
+          </li>
+        })}
+      </ul>
+
     </div>
   );
 }
