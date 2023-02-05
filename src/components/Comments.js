e6a566409e6cd38effa1f6550262a8fa69a53ebe
Ziru Zou <jiyucoding@yahoo.com>
2023-02-05 10:01:33 -0500

    set toggle state, onClick listener, render button context and toggle comment lists on conditions

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,16 +1,36 @@
+import { useState } from "react"
+
 function Comments({ comments }) {
+
+    const [toggle, setToggle] = useState(false)
+
+    const context = toggle ? "Hide Comments" : "Show Comments"
+
+    function handleToggleComments() {
+        setToggle(toggle => !toggle)
+    }
+
     return (
         <>
-            <button id="comments">Hide Comments</button>
-            <h2>{comments.length} comments</h2>
-            <ul >
-                {comments.map(comment => {
-                return <li key={comment.id} id={comment.id}>
-                    <h3>{comment.user}</h3>
-                    <p>{comment.comment}</p>
-                </li>
-                })}
-            </ul>
+            <button id="comment-btn" onClick={handleToggleComments}>
+                {context}
+            </button>
+            <div id="comment-lists">
+                <h2>{comments.length} comments</h2>
+                {toggle &&
+                    <ul >
+                    {comments.map(comment => {
+                    return <li key={comment.id} id={comment.id}>
+                        <h3>{comment.user}</h3>
+                        <p>{comment.comment}</p>
+                    </li>
+                    })}
+                </ul>
+                }
+                
+
+            </div>
+            
         </>
     )
 }
