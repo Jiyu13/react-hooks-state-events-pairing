16c0096bad8a6397159b109ad6f8660557465895
Ziru Zou <jiyucoding@yahoo.com>
2023-02-05 12:30:25 -0500

    add onClick event listener by calling the prop CB--onDeleteChange

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,10 +1,12 @@
 import VoteButtons from "./VoteButtons"
 
-function Comment({comment}) {
+function Comment({comment, onDeleteChange}) {
     return <li key={comment.id} id={comment.id}>
         <VoteButtons upvotes={0} downvotes={0}/>
         <h3>{comment.user}</h3>
         <p>{comment.comment}</p>
+
+        <button id={comment.user} name={comment.user} onClick={onDeleteChange}>Delete</button>
     </li>
         
 }
